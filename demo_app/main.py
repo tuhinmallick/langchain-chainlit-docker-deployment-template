5f3d0a8c3259346c6dd1f5d59c0b782426ad3cb4
Sourcery AI <>
2023-11-04 07:36:28 +0000

    'Refactored by Sourcery'

--- demo_app/main.py
+++ demo_app/main.py
@@ -21,6 +21,4 @@ def factory():
     user_env = cl.user_session.get("env")
     os.environ["OPENAI_API_KEY"] = user_env.get("OPENAI_API_KEY")
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
-
-    return llm_chain
+    return LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
